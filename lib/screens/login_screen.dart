bcabafd1a612d0dfcb350d5c4a925c8e335d7123
Jannes <jmuense@it-economics.de>
2022-09-21 10:14:02 +0200

    add spinner during login and register | refactor code

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/components/app_button_primary.dart';
 import 'package:flash_chat/components/alert_dialog.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 import 'chat_screen.dart';
 
@@ -17,76 +18,98 @@ class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
   String email;
   String password;
+  bool isAsyncCall = false;
+
+  Future<void> startLoginProcess(BuildContext context) async {
+    {
+      setState(() {
+        isAsyncCall = true;
+      });
+      try {
+        await loginWithFirebase(context);
+        password = null;
+        email = null;
+      } on FirebaseAuthException catch (e) {
+        await DialogBuilder.showAlertDialog(context, e.message);
+      } catch (e) {
+        print('unexcepted error: $e');
+      } finally {
+        setState(() {
+          isAsyncCall = false;
+        });
+      }
+    }
+  }
+
+  Future<void> loginWithFirebase(BuildContext context) async {
+    final newUser = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    if (newUser != null) {
+      Navigator.pushNamed(context, ChatScreen.id);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
-              ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 0.0),
-              child: TextField(
-                style: kInputTextStyle,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: kInputFieldDecoration.copyWith(
-                  hintText: 'Enter your Email',
+      body: ModalProgressHUD(
+        inAsyncCall: isAsyncCall,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
                 ),
               ),
-            ),
-            TextField(
-              style: kInputTextStyle,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: kInputFieldDecoration.copyWith(
-                hintText: 'Enter your Password',
+              SizedBox(
+                height: 48.0,
               ),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            Hero(
-              tag: 'login',
-              child: AppButtonPrimary(
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 0.0),
+                child: TextField(
+                  style: kInputTextStyle,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: kInputFieldDecoration.copyWith(
+                    hintText: 'Enter your Email',
+                  ),
+                ),
+              ),
+              TextField(
+                style: kInputTextStyle,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kInputFieldDecoration.copyWith(
+                  hintText: 'Enter your Password',
+                ),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              Hero(
+                tag: 'login',
+                child: AppButtonPrimary(
                   text: "Log In",
                   color: Colors.lightBlueAccent,
                   onTapFn: () async {
-                    try {
-                      print(email);
-                      print(password);
-                      final newUser = await _auth.signInWithEmailAndPassword(
-                        email: email,
-                        password: password,
-                      );
-                      if (newUser != null) {
-                        Navigator.pushNamed(context, ChatScreen.id);
-                      }
-                    } on FirebaseAuthException catch (e) {
-                      await DialogBuilder.showAlertDialog(context, e.message);
-                    } catch (e) {
-                      print('unexcepted error');
-                    }
-                  }),
-            ),
-          ],
+                    startLoginProcess(context);
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -3,6 +3,9 @@ import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+
+import '../components/alert_dialog.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static String id = 'registration';
@@ -13,74 +16,97 @@ class RegistrationScreen extends StatefulWidget {
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
   final _auth = FirebaseAuth.instance;
+  bool isInAsyncCall = false;
   String email;
   String password;
+
+  Future<void> startRegistrationProcess(BuildContext context) async {
+    setState(() {
+      isInAsyncCall = true;
+    });
+    try {
+      await registerWithFirebase(context);
+      email = null;
+      password = null;
+    } on FirebaseAuthException catch (e) {
+      await DialogBuilder.showAlertDialog(context, e.message);
+    } catch (e) {
+      print('unexcepted error: $e');
+    } finally {
+      setState(() {
+        isInAsyncCall = false;
+      });
+    }
+  }
+
+  Future<void> registerWithFirebase(BuildContext context) async {
+    final newUser = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    if (newUser != null) {
+      Navigator.pushNamed(context, ChatScreen.id);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: isInAsyncCall,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-                textAlign: TextAlign.center,
-                style: kInputTextStyle,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: kInputFieldDecoration.copyWith(
-                  hintText: 'Enter your Email',
-                )),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-                textAlign: TextAlign.center,
-                style: kInputTextStyle,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: kInputFieldDecoration.copyWith(
-                  hintText: 'Enter your Password',
-                )),
-            SizedBox(
-              height: 24.0,
-            ),
-            Hero(
-              tag: 'register',
-              child: AppButtonPrimary(
-                text: "Register",
-                color: Colors.blueAccent,
-                onTapFn: () async {
-                  try {
-                    final newUser = await _auth.createUserWithEmailAndPassword(
-                      email: email,
-                      password: password,
-                    );
-                    if (newUser != null) {
-                      Navigator.pushNamed(context, ChatScreen.id);
-                    }
-                  } catch (e) {
-                    print(e);
-                  }
-                },
+              SizedBox(
+                height: 48.0,
               ),
-            ),
-          ],
+              TextField(
+                  textAlign: TextAlign.center,
+                  style: kInputTextStyle,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: kInputFieldDecoration.copyWith(
+                    hintText: 'Enter your Email',
+                  )),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                  textAlign: TextAlign.center,
+                  style: kInputTextStyle,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: kInputFieldDecoration.copyWith(
+                    hintText: 'Enter your Password',
+                  )),
+              SizedBox(
+                height: 24.0,
+              ),
+              Hero(
+                tag: 'register',
+                child: AppButtonPrimary(
+                  text: "Register",
+                  color: Colors.blueAccent,
+                  onTapFn: () async {
+                    await startRegistrationProcess(context);
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
